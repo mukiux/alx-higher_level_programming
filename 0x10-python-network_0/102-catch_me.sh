f1dcc7a70a8f899bcd21005fef979ad06d336591
George Kiio <mukiux@gmail.com>
2024-04-25 08:04:45 -0700

    0x10-python-network_0

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Catch me if you can!
-curl -sL 0.0.0.0:5000/catch_me -X PUT -d "user_id=98" -H "Origin: School"
-
+# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
+curl -s -X PUT -d "user_id=98" -H "Origin: You got me!" -L 0.0.0.0:5000/catch_me
